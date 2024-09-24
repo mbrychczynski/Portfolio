561d4649c8e0a4c989d86a63f5446adc82a6fbe1
Mateusz Brychczynski <mat.brych@gmail.com>
2024-09-24 19:23:25 +0100

    Flask Templates

--- server.py
+++ server.py
@@ -1,8 +1,20 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def hello_world():
-    return "Hello, World!"
+    return render_template("index.html")
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+@app.route("/blog")
+def blog():
+    return "Blog"
+
+@app.route("/blog/2020/dogs")
+def blog2():
+    return "Blog2"
