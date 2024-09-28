5ef9c8f5c5065c8587575d5cf8e59031e3e8193c
Mateusz Brychczynski <mat.brych@gmail.com>
2024-09-29 00:03:19 +0100

    Setting up page using free HTML template

--- server.py
+++ server.py
@@ -2,14 +2,16 @@ from flask import Flask, render_template, url_for, send_from_directory
 
 app = Flask(__name__)
 
-@app.route("/<username>/<int:post_id>")
-def hello_world(username=None, post_id=None):
-    return render_template("index.html", name=username, post_id=post_id)
 
-@app.route("/about")
-def about():
-    return render_template("about.html")
+@app.route("/")
+def my_home():
+    return render_template("index.html")
 
-@app.route("/blog")
-def blog():
-    return "Blog"
+
+@app.route("/<string:page_name>")
+def html_page(page_name):
+    return render_template(page_name)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
